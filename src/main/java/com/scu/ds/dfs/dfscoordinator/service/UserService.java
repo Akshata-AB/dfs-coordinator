8c47a792dd62584120b14c23e6e5abce894beff7
Akshata-AB <akshatabhonsle27@gmail.com>
2023-03-16 17:58:47 -0700

    Upload,download + Java files

--- src/main/java/com/scu/ds/dfs/dfscoordinator/service/UserService.java
+++ src/main/java/com/scu/ds/dfs/dfscoordinator/service/UserService.java
@@ -1,7 +1,10 @@
 package com.scu.ds.dfs.dfscoordinator.service;
 
 import com.scu.ds.dfs.dfscoordinator.model.User;
+import org.springframework.stereotype.Component;
 
+
+@Component
 public interface UserService {
 
     User createProfile(User user);
